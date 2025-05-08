7f6eac4e363d206bfb75f8f0211925a63c2e6e20
Stephane Zermatten <szermatt@gmx.net>
2025-05-08 13:31:53 +0300

    WIP

--- src/model/service.rs
+++ src/model/service.rs
@@ -1,39 +1,40 @@
 //! Service definition for Realize - Symmetric File Syncer
 //!
 //! This module defines the RealizeService trait for use with tarpc 0.36.
-//! It exposes the following methods:
-//! - List
-//! - Send
-//! - Finish
-//!
-//! See spec/design.md and spec/future.md for details.
 
+use crate::model::error::Result;
 use std::path::PathBuf;
 
 pub type DirectoryId = String;
 pub type ByteRange = (u64, u64);
-pub type Result<T> = std::result::Result<T, ()>;
 
 #[derive(Debug, Clone)]
 pub struct SyncedFile {
     pub path: PathBuf,
-    pub size: usize,
-    pub is_partial: bool,
+    pub size: u64,
+    pub state: SyncedFileState,
+}
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+pub enum SyncedFileState {
+    Final,
+    Partial,
 }
 
 /// The service trait for file synchronization.
-///
-/// Methods:
-/// - List(DirectoryId) -> Result<Vec<SyncedFile>>
-/// - Send(DirectoryId, Path, ByteRange, Data) -> Result<()> 
-/// - Finish(DirectoryId, Path) -> Result<()> 
 pub trait RealizeService {
     // List files in a directory
     fn list(&self, dir_id: DirectoryId) -> Result<Vec<SyncedFile>>;
 
     // Send a byte range of a file
-    fn send(&self, dir_id: DirectoryId, path: PathBuf, range: ByteRange, data: Vec<u8>) -> Result<()>;
+    fn send(
+        &self,
+        dir_id: DirectoryId,
+        relative_path: PathBuf,
+        range: ByteRange,
+        data: Vec<u8>,
+    ) -> Result<()>;
 
     // Mark a partial file as complete
-    fn finish(&self, dir_id: DirectoryId, path: PathBuf) -> Result<()>;
-} 
+    fn finish(&self, dir_id: DirectoryId, relative_path: PathBuf) -> Result<()>;
+}
--- src/server.rs
+++ src/server.rs
@@ -1,56 +1,322 @@
-use crate::model::service::RealizeService;
-use std::path::PathBuf;
+use crate::model::error::{RealizeError, Result};
+use crate::model::service::{ByteRange, DirectoryId, RealizeService, SyncedFile, SyncedFileState};
+use std::collections::HashMap;
+use std::ffi::OsString;
+use std::fs::{self, OpenOptions};
+use std::io::{Seek, SeekFrom, Write};
+use std::os::unix::fs::MetadataExt as _;
+use std::path::{Path, PathBuf};
+use std::rc::Rc;
+use walkdir::WalkDir;
 
-// Dummy type definitions for compilation
-pub type DirectoryId = String;
-pub type ByteRange = (u64, u64);
-pub type Result<T> = std::result::Result<T, ()>;
+pub struct RealizeServer {
+    /// Maps directory IDs to local paths
+    dirs: HashMap<DirectoryId, Rc<Directory>>,
+}
 
-pub struct DummyService;
-
-impl RealizeService for DummyService {
-    // List files in a directory
-    fn list(&self, _dir_id: DirectoryId) -> Result<Vec<SyncedFile>> {
-        Ok(vec![])
+impl RealizeServer {
+    pub fn new<T>(dirs: T) -> Self
+    where
+        T: IntoIterator<Item = Directory>,
+    {
+        Self {
+            dirs: dirs
+                .into_iter()
+                .map(|dir| (dir.id.clone(), Rc::new(dir)))
+                .collect(),
+        }
     }
 
-    // Send a byte range of a file
-    fn send(&self, _dir_id: DirectoryId, _path: PathBuf, _range: ByteRange, _data: Vec<u8>) -> Result<()> {
+    fn find_directory(&self, dir_id: &DirectoryId) -> Result<&Rc<Directory>> {
+        self.dirs
+            .get(dir_id)
+            .ok_or_else(|| RealizeError::BadRequest(format!("Unknown directory ID: '{}'", dir_id)))
+    }
+}
+
+impl RealizeService for RealizeServer {
+    fn list(&self, dir_id: DirectoryId) -> Result<Vec<SyncedFile>> {
+        let dir = self.find_directory(&dir_id)?;
+        let mut files = Vec::new();
+        for entry in WalkDir::new(dir.path()).into_iter().flatten() {
+            if let Some((state, logical)) = LogicalPath::from_actual(&dir, entry.path()) {
+                // TODO: What if a file exists for both the partial
+                // and final form of the logical path? Only one entry
+                // should be returned and its state should be final.
+                files.push(SyncedFile {
+                    path: logical.relative_path().to_path_buf(),
+                    size: entry.metadata()?.size(),
+                    state,
+                });
+            }
+        }
+
+        Ok(files)
+    }
+
+    fn send(
+        &self,
+        dir_id: DirectoryId,
+        relative_path: PathBuf,
+        range: ByteRange,
+        data: Vec<u8>,
+    ) -> Result<()> {
+        let dir = self.find_directory(&dir_id)?;
+        let logical = LogicalPath::new(dir, &relative_path);
+        let path = logical.partial_path();
+        if let Ok((state, actual)) = logical.find() {
+            // File already exists
+            if state == SyncedFileState::Final {
+                fs::rename(actual, &path)?;
+            }
+        } else {
+            // Open will need to create the file. Ensure parent
+            // directory exists beforehand.
+            if let Some(parent) = path.parent() {
+                fs::create_dir_all(parent)?;
+            }
+        }
+        let mut file = OpenOptions::new().create(true).write(true).open(&path)?;
+        file.seek(SeekFrom::Start(range.0))?;
+        file.write_all(&data)?;
+
         Ok(())
     }
 
-    // Mark a partial file as complete
-    fn finish(&self, _dir_id: DirectoryId, _path: PathBuf) -> Result<()> {
+    fn finish(&self, dir_id: DirectoryId, relative_path: PathBuf) -> Result<()> {
+        let dir = self.find_directory(&dir_id)?;
+        let logical = LogicalPath::new(dir, &relative_path);
+        if let (SyncedFileState::Partial, real_path) = logical.find()? {
+            fs::rename(real_path, logical.final_path())?;
+        }
         Ok(())
+    }
+}
+
+// A directory, stored in RealizeServer and in LogicalPath.
+pub struct Directory {
+    id: DirectoryId,
+    path: PathBuf,
+}
+
+impl Directory {
+    /// Create a directory with the given id and path.
+    pub fn new(id: &DirectoryId, path: &Path) -> Self {
+        Self {
+            id: id.clone(),
+            path: path.to_path_buf(),
+        }
+    }
+
+    /// Directory ID, as found in Service calls.
+    pub fn id(&self) -> &DirectoryId {
+        &self.id
+    }
+
+    /// Local directory path that correspond to the ID.
+    pub fn path(&self) -> &Path {
+        &self.path
+    }
+}
+
+/// A logical path is a relative path inside of a [Directory].
+//
+/// The corresponding actual file might be in
+/// [SyncedFileState::Partial] or [SyncedFileState::Final] form. Check
+/// with [LogicalPath::find].
+struct LogicalPath(Rc<Directory>, PathBuf);
+
+impl LogicalPath {
+    /// Create a new logical path, without checking it.
+    fn new(dir: &Rc<Directory>, path: &Path) -> Self {
+        // TODO: check that path is relative and doesn't use any ..
+        Self(Rc::clone(dir), path.to_path_buf())
+    }
+
+    /// Create a logical path from an actual partial or final path.
+    fn from_actual(dir: &Rc<Directory>, actual: &Path) -> Option<(SyncedFileState, Self)> {
+        if !actual.is_file() {
+            return None;
+        }
+
+        let relative = pathdiff::diff_paths(actual, dir.path());
+        if let Some(mut relative) = relative {
+            if let Some(filename) = actual.file_name() {
+                let name_bytes = filename.to_string_lossy();
+                if name_bytes.starts_with(".") && name_bytes.ends_with(".part") {
+                    let stripped_name =
+                        OsString::from(name_bytes[1..name_bytes.len() - 5].to_string());
+                    relative.set_file_name(&stripped_name);
+                    return Some((SyncedFileState::Partial, LogicalPath::new(dir, &relative)));
+                }
+                return Some((SyncedFileState::Final, LogicalPath::new(dir, &relative)));
+            }
+        }
+        return None;
+    }
+
+    // Look for a file for the logical path.
+    //
+    // The path can be found in final or partial found. Return both
+    // the [SyncedFileState] and the actual path of the file that was
+    // found.
+    //
+    // File with an I/O error of kind [std::io::ErrorKind::NotFound]
+    // if no file exists for the logical path.
+    fn find(&self) -> Result<(SyncedFileState, PathBuf)> {
+        let fpath = self.final_path();
+        if fpath.exists() {
+            return Ok((SyncedFileState::Final, fpath));
+        }
+        let partial = self.partial_path();
+        if partial.exists() {
+            return Ok((SyncedFileState::Partial, partial));
+        }
+
+        return Err(std::io::Error::from(std::io::ErrorKind::NotFound).into());
+    }
+
+    /// Return the final form of the logical path.
+    fn final_path(&self) -> PathBuf {
+        self.0.path().join(self.1.to_path_buf())
+    }
+
+    /// Return the partial form of the logical path.
+    fn partial_path(&self) -> PathBuf {
+        let mut partial = self.0.path().to_path_buf();
+        if let Some(p) = self.1.parent() {
+            partial.push(p);
+        }
+        let mut part_filename = OsString::from(".");
+        if let Some(fname) = self.1.file_name() {
+            part_filename.push(OsString::from(fname.to_string_lossy().to_string()));
+        }
+        part_filename.push(OsString::from(".part"));
+        partial.set_file_name(part_filename);
+
+        partial
+    }
+
+    /// Return the relative path of the logical path.
+    fn relative_path(&self) -> &Path {
+        &self.1
+    }
+}
+
+impl From<walkdir::Error> for RealizeError {
+    fn from(err: walkdir::Error) -> Self {
+        if let Some(_) = err.io_error() {
+            // We know err contains an io error; unwrap will succeed.
+            err.into_io_error().unwrap().into()
+        } else {
+            anyhow::Error::new(err).into()
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use tarpc::context;
-    use tarpc::server::{self, Channel};
-    use tarpc::transport::channel;
-    use futures::prelude::*;
-    use tokio::runtime::Runtime;
+    use assert_fs::prelude::*;
+    use assert_fs::TempDir;
+    use std::collections::HashMap;
+    use std::fs;
+    use std::io::Read;
 
-    #[tokio::test]
-    async fn test_dummy_service_inprocess_channel() {
-        // Create the server
-        let server = DummyService;
-        let (client_transport, server_transport) = channel::unbounded();
-
-        // Spawn the server
-        tokio::spawn(async move {
-            server::BaseChannel::with_defaults(server_transport)
-                .execute(server)
-                .await;
-        });
-
-        // Create a client (just call the trait directly for now)
-        // In a real tarpc service, you'd use a generated client stub.
-        // Here, we just call the DummyService directly for demonstration.
-        let result = server.list("test_dir".to_string());
-        assert_eq!(result.unwrap(), vec![]);
+    fn setup_server_with_dir() -> (RealizeServer, TempDir, DirectoryId) {
+        let temp = TempDir::new().unwrap();
+        let dir_id = "testdir".to_string();
+        let mut dirs = HashMap::new();
+        dirs.insert(dir_id.clone(), temp.path().to_path_buf());
+        (RealizeServer::new(dirs), temp, dir_id)
     }
-} 
+
+    #[test]
+    fn test_list_empty() {
+        let (server, _temp, dir_id) = setup_server_with_dir();
+        let files = server.list(dir_id).unwrap();
+        assert!(files.is_empty());
+    }
+
+    #[test]
+    fn test_list_files_and_partial() {
+        let (server, temp, dir_id) = setup_server_with_dir();
+        let file = temp.child("foo.txt");
+        file.write_str("hello").unwrap();
+        let part_file = temp.child("bar.txt.part");
+        part_file.write_str("partial").unwrap();
+        let files = server.list(dir_id).unwrap();
+        let mut found_foo = false;
+        let mut found_bar = false;
+        for f in files {
+            if f.path == PathBuf::from("foo.txt") {
+                assert!(!f.is_partial);
+                found_foo = true;
+            }
+            if f.path == PathBuf::from("bar.txt") {
+                assert!(f.is_partial);
+                found_bar = true;
+            }
+            // Path must not start with '.' or end with '.part'
+            assert!(!f.path.to_string_lossy().starts_with("."));
+            assert!(!f.path.to_string_lossy().ends_with(".part"));
+        }
+        assert!(found_foo);
+        assert!(found_bar);
+    }
+
+    #[test]
+    fn test_send_and_finish_partial() {
+        let (server, temp, dir_id) = setup_server_with_dir();
+        let data = b"abcde".to_vec();
+        let path = PathBuf::from("file1.txt");
+        // Send to partial file
+        server
+            .send(dir_id.clone(), path.clone(), (0, 5), data.clone())
+            .unwrap();
+        let part_path = temp.path().join("file1.txt.part");
+        assert!(part_path.exists());
+        let mut buf = Vec::new();
+        fs::File::open(&part_path)
+            .unwrap()
+            .read_to_end(&mut buf)
+            .unwrap();
+        assert_eq!(buf, data);
+        // Finish (rename to non-partial)
+        server.finish(dir_id.clone(), path.clone()).unwrap();
+        let final_path = temp.path().join("file1.txt");
+        assert!(final_path.exists());
+        assert!(!part_path.exists());
+        let mut buf2 = Vec::new();
+        fs::File::open(&final_path)
+            .unwrap()
+            .read_to_end(&mut buf2)
+            .unwrap();
+        assert_eq!(buf2, data);
+    }
+
+    #[test]
+    fn test_send_and_finish_on_existing_nonpartial() {
+        let (server, temp, dir_id) = setup_server_with_dir();
+        let orig_data = b"orig";
+        let path = PathBuf::from("file2.txt");
+        let final_path = temp.path().join("file2.txt");
+        fs::write(&final_path, orig_data).unwrap();
+        // Send new data (should write to .part)
+        let new_data = b"newdata".to_vec();
+        server
+            .send(dir_id.clone(), path.clone(), (0, 7), new_data.clone())
+            .unwrap();
+        let part_path = temp.path().join("file2.txt.part");
+        assert!(part_path.exists());
+        // Finish (should overwrite non-partial)
+        server.finish(dir_id.clone(), path.clone()).unwrap();
+        assert!(final_path.exists());
+        let mut buf = Vec::new();
+        fs::File::open(&final_path)
+            .unwrap()
+            .read_to_end(&mut buf)
+            .unwrap();
+        assert_eq!(buf, new_data);
+    }
+}
