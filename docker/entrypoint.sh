67cdf0e2e66e2142e5b83ad6e92d343788bf7a28
Stephane Zermatten <szermatt@gmx.net>
2025-05-17 21:24:20 +0300

    chore: Fix metrics address env var in Dockerfile

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 exec /usr/local/bin/realized \
   --address "${ADDRESS}" \
-  --metrics-addr "${ADDRESS}" \
+  --metrics-addr "${METRICS_ADDRESS}" \
   --privkey "${PRIVKEY}" \
   --config "${CONFIG}"
