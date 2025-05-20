bdaa3ad75c203eab84d4f7aec684c55b5605ba1b
Stephane Zermatten <szermatt@gmx.net>
2025-05-20 21:13:16 +0300

    test: unit tests for reconnect

--- src/client/reconnect.rs
+++ src/client/reconnect.rs
@@ -12,6 +12,12 @@ pub trait Connect<T>: Clone {
     async fn run(&self) -> anyhow::Result<T>;
 }
 
+impl<T, F: (AsyncFn() -> anyhow::Result<T>) + Clone> Connect<T> for F {
+    async fn run(&self) -> anyhow::Result<T> {
+        self().await
+    }
+}
+
 #[derive(Clone)]
 pub struct Reconnect<T, C: Connect<T>, S> {
     inner: Arc<RwLock<Reconnectable<T>>>,
@@ -161,5 +167,233 @@ fn should_reconnect<T>(result: &Result<T, RpcError>) -> bool {
         Err(RpcError::Shutdown) => true,
         Err(RpcError::Channel(_)) => true,
         _ => false,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{
+        collections::HashSet,
+        sync::atomic::{AtomicU32, Ordering},
+        time::Duration,
+    };
+
+    use futures::future::join_all;
+    use tarpc::{
+        ChannelError,
+        context::{self, Context},
+    };
+    use tokio_retry::strategy::ExponentialBackoff;
+
+    use super::*;
+
+    #[derive(Clone, Debug)]
+    struct FakeError {}
+
+    impl std::error::Error for FakeError {}
+    impl std::fmt::Display for FakeError {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            f.write_str("fake")
+        }
+    }
+
+    #[derive(Clone)]
+    struct TestStub {
+        max_count: u32,
+        counter: Arc<AtomicU32>,
+    }
+
+    impl TestStub {
+        fn new(max_count: u32) -> TestStub {
+            Self {
+                max_count,
+                counter: Arc::new(AtomicU32::new(0)),
+            }
+        }
+    }
+
+    impl Stub for TestStub {
+        type Req = Arc<String>;
+        type Resp = String;
+
+        async fn call(
+            &self,
+            _ctx: Context,
+            req: Arc<String>,
+        ) -> std::result::Result<String, RpcError> {
+            let count = self.counter.fetch_add(1, Ordering::Relaxed);
+            if count == self.max_count {
+                return Err(RpcError::Channel(ChannelError::Write(Arc::new(
+                    FakeError {},
+                ))));
+            }
+            if count > self.max_count {
+                return Err(RpcError::Shutdown);
+            }
+            if *req == "ping" {
+                Ok("pong".to_string())
+            } else {
+                Ok("what?".to_string())
+            }
+        }
+    }
+
+    #[derive(Clone)]
+    struct TestStubConnect {
+        counter: Arc<AtomicU32>,
+        fail: HashSet<u32>,
+    }
+    impl TestStubConnect {
+        fn new(counter: Arc<AtomicU32>) -> TestStubConnect {
+            Self {
+                counter,
+                fail: HashSet::new(),
+            }
+        }
+
+        fn add_failure(&mut self, call_count: u32) {
+            self.fail.insert(call_count);
+        }
+    }
+    impl Connect<TestStub> for TestStubConnect {
+        async fn run(&self) -> anyhow::Result<TestStub> {
+            let count = self.counter.fetch_add(1, Ordering::Relaxed);
+
+            if self.fail.contains(&count) {
+                return Err(anyhow::anyhow!("connection {count} failed"));
+            }
+            Ok(TestStub::new(2))
+        }
+    }
+
+    #[tokio::test]
+    async fn reconnect() -> anyhow::Result<()> {
+        let strategy = ExponentialBackoff::from_millis(1);
+        let connection_count = Arc::new(AtomicU32::new(0));
+        let reconnect = Reconnect::new(
+            strategy,
+            TestStubConnect::new(Arc::clone(&connection_count)),
+        )
+        .await?;
+
+        for _ in 0..10 {
+            assert_eq!(
+                "pong",
+                reconnect
+                    .call(context::current(), "ping".to_string())
+                    .await?
+            );
+        }
+
+        assert_eq!(5, connection_count.load(Ordering::Relaxed));
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn reconnect_parallel() -> anyhow::Result<()> {
+        let strategy = ExponentialBackoff::from_millis(1);
+        let connection_count = Arc::new(AtomicU32::new(0));
+        let reconnect = Reconnect::new(
+            strategy,
+            TestStubConnect::new(Arc::clone(&connection_count)),
+        )
+        .await?;
+
+        // Warm up the stub
+        assert_eq!(
+            "pong",
+            reconnect
+                .call(context::current(), "ping".to_string())
+                .await?
+        );
+
+        // Execute requests in parallel. All must (eventually) go through.
+        for res in
+            join_all((0..19).map(|_| reconnect.call(context::current(), "ping".to_string()))).await
+        {
+            assert_eq!("pong".to_string(), res?);
+        }
+
+        assert_eq!(10, connection_count.load(Ordering::Relaxed));
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn reconnect_sometimes_fail() -> anyhow::Result<()> {
+        let strategy = ExponentialBackoff::from_millis(1);
+        let connection_count = Arc::new(AtomicU32::new(0));
+
+        // Connecting fails 4 times, including 3 consecutive failures
+        let mut connect = TestStubConnect::new(Arc::clone(&connection_count));
+        connect.add_failure(1);
+        connect.add_failure(3);
+        connect.add_failure(4);
+        connect.add_failure(5);
+        let reconnect = Reconnect::new(strategy, connect).await?;
+
+        for _ in 0..10 {
+            assert_eq!(
+                "pong",
+                reconnect
+                    .call(context::current(), "ping".to_string())
+                    .await?
+            );
+        }
+
+        // 5 successes + 4 failures
+        assert_eq!(9, connection_count.load(Ordering::Relaxed));
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn reconnect_gives_up() -> anyhow::Result<()> {
+        let strategy = vec![Duration::from_millis(1), Duration::from_millis(1)].into_iter();
+        let connection_count = Arc::new(AtomicU32::new(0));
+
+        // Connecting fails 4 times, including 3 consecutive failures, while the strategy only
+        // allows for 2 consecutive failures.
+        let mut connect = TestStubConnect::new(Arc::clone(&connection_count));
+        connect.add_failure(1);
+        connect.add_failure(3);
+        connect.add_failure(4);
+        connect.add_failure(5);
+        let reconnect = Reconnect::new(strategy, connect).await?;
+
+        // The first 4 calls work, with one successful reconnection.
+        for _ in 0..4 {
+            assert_eq!(
+                "pong",
+                reconnect
+                    .call(context::current(), "ping".to_string())
+                    .await?
+            );
+        }
+        // initial connection + 1 reconnection + 1 retried connection
+        // failure
+        assert_eq!(3, connection_count.load(Ordering::Relaxed));
+
+        // After that reconnecting fails and eventually reconnect
+        // gives up.
+        assert!(matches!(
+            reconnect.call(context::current(), "ping".to_string()).await,
+            Err(RpcError::Shutdown),
+        ));
+
+        // 2 more than last time: tried to reconnect twice and then gave up.
+        assert_eq!(5, connection_count.load(Ordering::Relaxed));
+
+        // no more reconnection attempts
+        for _ in 0..3 {
+            assert!(matches!(
+                reconnect.call(context::current(), "ping".to_string()).await,
+                Err(RpcError::Shutdown),
+            ));
+        }
+        assert_eq!(5, connection_count.load(Ordering::Relaxed));
+
+        Ok(())
     }
 }
