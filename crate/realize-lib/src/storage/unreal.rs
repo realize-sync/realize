1af93866b1940308bfdf58bb81f284de95efc9a8
Stephane Zermatten <szermatt@gmx.net>
2025-06-21 23:47:42 +0300

    refactor: move cache types from unreal::cache to unreal.
    This way, cache types can be non-public but still usable in sub-modules
    of unreal.

--- crate/realize-lib/src/storage/unreal.rs
+++ crate/realize-lib/src/storage/unreal.rs
@@ -1,4 +1,7 @@
-mod cache;
+use crate::model::{self};
+use redb::Value;
+use std::time::SystemTime;
+
 mod downloader;
 mod error;
 mod future;
@@ -10,14 +13,102 @@ mod unreal_capnp {
     include!(concat!(env!("OUT_DIR"), "/unreal_capnp.rs"));
 }
 
-pub use cache::FileEntry;
-pub use cache::FileMetadata;
-pub use cache::InodeAssignment;
-pub use cache::ReadDirEntry;
-pub use cache::ROOT_DIR;
 pub use downloader::Download;
 pub use downloader::Downloader;
 pub use error::UnrealCacheError;
 pub use future::UnrealCacheAsync;
 pub use sync::UnrealCacheBlocking;
 pub use updater::keep_cache_updated;
+
+/// Inode of the root dir.
+pub const ROOT_DIR: u64 = 1;
+
+/// An entry in a directory listing.
+#[derive(Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
+pub struct ReadDirEntry {
+    /// The inode of the entry.
+    pub inode: u64,
+    /// The type of the entry.
+    pub assignment: InodeAssignment,
+}
+
+/// The type of an inode.
+#[derive(Debug, Copy, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
+pub enum InodeAssignment {
+    /// The inode of a file, look it up in the file table.
+    File,
+    /// The inode of a directory, look it up in the directory table.
+    ///
+    /// Note that an empty directory won't have any entries in
+    /// the directory table.
+    Directory,
+}
+
+/// An entry in the file table.
+#[derive(Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
+pub struct FileEntry {
+    /// The arena to use to fetch file content in the peer.
+    ///
+    /// This is stored here as a key to fetch file content,
+    /// to be replaced by a blob id.
+    pub arena: model::Arena,
+
+    /// The path to use to fetch file content in the peer.
+    ///
+    /// Note that it shouldn't matter whether the path
+    /// here matches the path which led to this file. This
+    /// is to be treated as a key for downloading and nothing else..
+    ///
+    /// This is stored here as a key to fetch file content,
+    /// to be replaced by a blob id.
+    pub path: model::Path,
+
+    /// The metadata of the file.
+    pub metadata: FileMetadata,
+
+    /// Inode of the containing directory
+    pub(crate) parent_inode: u64,
+}
+
+impl Value for FileEntry {
+    type SelfType<'a>
+        = FileEntry
+    where
+        Self: 'a;
+
+    type AsBytes<'a>
+        = Vec<u8>
+    where
+        Self: 'a;
+
+    fn fixed_width() -> Option<usize> {
+        None
+    }
+
+    fn from_bytes<'a>(data: &'a [u8]) -> Self::SelfType<'a>
+    where
+        Self: 'a,
+    {
+        bincode::deserialize::<FileEntry>(data).unwrap()
+    }
+
+    fn as_bytes<'a, 'b: 'a>(value: &'a Self::SelfType<'b>) -> Self::AsBytes<'a>
+    where
+        Self: 'b,
+    {
+        bincode::serialize(value).unwrap()
+    }
+
+    fn type_name() -> redb::TypeName {
+        redb::TypeName::new("FileEntry")
+    }
+}
+
+/// The metadata of a file.
+#[derive(Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
+pub struct FileMetadata {
+    /// The size of the file in bytes.
+    pub size: u64,
+    /// The modification time of the file.
+    pub mtime: SystemTime,
+}
